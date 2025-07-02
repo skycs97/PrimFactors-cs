2f7273beea96791b2b8e4ddf6c9a619162569525
skycs97 <skycs97@gmail.com>
2025-07-02 10:10:34 +0900

    [feature] Page 56.

--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -23,7 +23,13 @@ TEST_F(PrimeFixture, Of3) {
 	expected = {3};
 	EXPECT_EQ(expected, prime_factor.of(3));
 }
+
 TEST_F(PrimeFixture, Of4) {
 	expected = {2, 2};
 	EXPECT_EQ(expected, prime_factor.of(4));
+}
+
+TEST_F(PrimeFixture, Of6) {
+	expected = {2, 3};
+	EXPECT_EQ(expected, prime_factor.of(6));
 }
